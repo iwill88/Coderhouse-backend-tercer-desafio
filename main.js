78a540d86464affd74c11a7dc409d1f3b5879341
AlexanderC <alex88_upt@hotmail.com>
2022-10-24 17:23:44 -0500

    tercer desafio server

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const fs=require('fs');
+const fs = require("fs");
 
 const express = require("express");
 
@@ -6,75 +6,60 @@ const PORT = 8080;
 
 const app = express();
 
+const server = app.listen(PORT, () => {
+          console.log(`App escuchando el puerto ${server.address().port}`);
+        });
 
 class Contenedor {
-    constructor(nombre) {
-        this.nombre = nombre
-        this.id = 1
-        this.contenido=""
+  constructor(nombre) {
+    this.nombre = nombre;
+    this.id = 1;
+    this.contenido = "";
+  }
+
+  async getRandom() {
+    try {
+        let contenido = await fs.promises.readFile(this.nombre, "utf-8")
+        contenido= JSON.parse(contenido);
+        const id = Math.floor(Math.random() * 3) + 1
+          let seleccion = contenido.filter((e) => e.id == id);
+          return seleccion
+    } catch (err) {
+      console.log("Hubo un error no se subio nada al servidor 28", err);
     }
-    
-    async getRandom(id){ 
-        await fs.readFile(this.nombre,"utf-8",
-        (err, contenido)=>{
-            if (err) {
-                console.log("Hubo un error no se subio nada al servidor",err)
-            } else {
-                contenido=JSON.parse(contenido)
-                let seleccion=contenido.filter(e=>e.id==id)
-                app.get("/productoRandom", (req, res) => {
-
-                    console.log(req.query);
-                  
-                    res.send(seleccion);
-                  
-                  });
-                
-                  const server = app.listen(PORT, () => {
-                
-                    console.log(`App escuchando el puerto ${server.address().port}`);
-                  
-                  });
-                  
-                  server.on("error", (error) => console.log(error.message));
 
 
+        
+  }
 
-            }
-        }
-        )
-    }
-    
-    async getAll(){
-        await fs.readFile(this.nombre,"utf-8",
-        (err, contenido)=>{
-            if (err) {
-                console.log("Hubo un error no se subio nada al servidor",err)
-            } else {
-                app.get("/productos", (req, res) => {
-
-                    console.log(req.query);
-                  
-                    res.send(contenido);
-                  
-                  });
-                
-                  const server = app.listen(PORT, () => {
-                
-                    console.log(`App escuchando el puerto ${server.address().port}`);
-                  
-                  });
-                  
-                  server.on("error", (error) => console.log(error.message));
-            }
-        }
-        )
-    }
-    
+  async getAll() {
+    try {
+      let contenido = await fs.promises.readFile(this.nombre, "utf-8")
+      contenido= JSON.parse(contenido);
+      return contenido
+  } catch (err) {
+    console.log("Hubo un error no se subio nada al servidor 28", err);
+  }
+  }
 }
 
-const nuevoContenedor =new Contenedor ('./productos.txt')
+const nuevoContenedor = new Contenedor("./productos.txt");
 
-nuevoContenedor.getAll();
-nuevoContenedor.getRandom(Math.floor(Math.random() * 3) + 1);
+
+
+
+
+app.get("/productoRandom", async (req, res) => {
+  const contenido = await nuevoContenedor.getRandom();
+  
+  res.send(contenido);
+ 
+});
+
+app.get("/productos", async (req, res) => {
+  const contenido = await nuevoContenedor.getAll();
+  
+  res.send(contenido);
+ 
+});
 
